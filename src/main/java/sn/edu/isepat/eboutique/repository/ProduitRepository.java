1c43ae2d6e44b265f06529e9cf218d57c73d4fa4
seynabou517 <mbengueseyny@gmail.com>
2026-02-20 11:38:20 +0100

    initial commit :categorie init

--- src/main/java/sn/edu/isepat/eboutique/repository/ProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package sn.edu.isepat.eboutique.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sn.edu.isepat.eboutique.entities.ProduitSeynabouMbengue;
-
-public interface ProduitRepository extends JpaRepository<ProduitSeynabouMbengue, Long> {
-}
